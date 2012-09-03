ad3e761fb066a610f3587b43df971f1b296f5780
sailxjx <sailxjx@163.com>
2012-09-03 16:34:30 +0800

    add queue and table model

--- app-notice/queue/Queue.php
+++ app-notice/queue/Queue.php
@@ -8,102 +8,102 @@
  */
 abstract class Queue_Queue extends Model_Base {
 
-	protected $oRedis;
-	protected $sQueue;
-	protected $aWait;
-	protected $aError;
-	protected $aSucc;
-	protected $aFail;
-	protected $aQueues = array(
-		'wait', //正常等待队列
-		'succ', //发送成功队列
-		'error', //发送错误队列
-		'fail'//发送失败队列
-	);
+    protected $oRedis;
+    protected $sQueue;
+    protected $aQueues = array(
+        'wait' => array(), //正常等待队列
+        'succ' => array(), //发送成功队列
+        'error' => array(), //发送错误队列
+        'fail' => array() //发送失败队列
+    );
 
-	public function __construct() {
-		if (!isset($this->sQueue)) {
-			trigger_error('code: don\'t forget to set the key of queue', E_USER_ERROR);
-		}
-		$this->oRedis = Fac_Db::getIns()->loadRedis();
-	}
+    public function __construct() {
+        $this->getQueue();
+        $this->oRedis = Fac_Db::getIns()->loadRedis();
+    }
 
-	/**
-	 * 初始化队列
-	 * @return \Queue_Queue
-	 */
-	protected function init() {
-		foreach ($this->aQueues as $sQue) {
-			$sQue = 'a' . ucfirst($sQue);
-			if (isset($this->$sQue)) {
-				$this->$sQue = null;
-			}
-		}
-		return $this;
-	}
+    /**
+     * 初始化队列
+     * @return \Queue_Queue
+     */
+    protected function init() {
+        foreach ($this->aQueues as $sQue => $aQue) {
+            $this->aQueues[$sQue] = array();
+        }
+        return $this;
+    }
 
-	/**
-	 *
-	 * @param string $name
-	 * @param array $args
-	 * @return boolean|\Queue_Queue
-	 */
-	public function __call($name, $args) {
-		$name = strtolower($name);
-		if (!in_array($name, $this->aQueues)) {
-			trigger_error('code: called method not found', E_USER_ERROR);
-			return false;
-		}
-		$this->setQueue($name, $args);
-		return $this;
-	}
+    /**
+     *
+     * @param string $name
+     * @param array $args
+     * @return boolean|\Queue_Queue
+     */
+    public function __call($name, $args) {
+        $name = strtolower($name);
+        if (!key_exists($name, $this->aQueues)) {
+            trigger_error('code: called method not found', E_USER_ERROR);
+            return false;
+        }
+        $this->setQueue($name, $args);
+        return $this;
+    }
 
-	/**
-	 * 进队列
-	 */
-	public function push() {
-		$this->oRedis->multi();
-		foreach ($this->aQueues as $sQue) {
-			$sPQue = 'a' . ucfirst($sQue);
-			$sKFunc = $this->sQueue . ucfirst($sQue);
-			if (isset($this->$sPQue)) {
-				foreach ($this->$sPQue as $id => $t) {
-					$this->oRedis->zadd(Redis_Key::$sKFunc(), $t, $id);
-				}
-			}
-		}
-		$this->oRedis->exec();
-		$this->init();
-		return $this;
-	}
+    /**
+     * 进队列
+     */
+    public function push() {
+        $this->oRedis->multi();
+        foreach ($this->aQueues as $sQue => $aQue) {
+            $sKFunc = $this->sQueue . ucfirst($sQue);
+            foreach ($aQue as $id => $t) {
+                $this->oRedis->zadd(Redis_Key::$sKFunc(), $t, $id);
+            }
+        }
+        $this->oRedis->exec();
+        $this->init();
+        return $this;
+    }
 
-	/**
-	 * 出队列
-	 */
-	public function pop() {
+    /**
+     * 出队列
+     */
+    public function pop() {
+        
+    }
 
-	}
+    /**
+     * 获取queue名
+     * @return string
+     */
+    protected function getQueue() {
+        if (!isset($this->sQueue)) {
+            list($sPre, $sQueue) = explode('_', get_called_class());
+            if (empty($sQueue)) {
+                trigger_error('could not find the called queue', E_USER_ERROR);
+            }
+            $this->sQueue = strtolower($sQueue);
+        }
+        return $this->sQueue;
+    }
 
-	/**
-	 * 设置队列值
-	 * @param string $name
-	 * @param array $args
-	 * @return boolean
-	 */
-	protected function setQueue($name, $args) {
-		$sQue = 'a' . ucfirst($name);
-		if (is_array($args[0])) {
-			foreach ($args[0] as $id => $t) {
-				$this->{$sQue}[$id] = $t;
-			}
-		}
-		elseif (isset($args[1])) {
-			$this->{$sQue}[$args[0]] = $args[1];
-		}
-		else {
-			return false;
-		}
-		return true;
-	}
+    /**
+     * 设置队列值
+     * @param string $name
+     * @param array $args
+     * @return boolean
+     */
+    protected function setQueue($name, $args) {
+        if (is_array($args[0])) {
+            foreach ($args[0] as $id => $t) {
+                $this->aQueues[$name][$id] = $t;
+            }
+        } elseif (isset($args[1])) {
+            $this->aQueues[$name][$args[0]] = $args[1];
+        } else {
+            return false;
+        }
+        return true;
+    }
 
 }
--- app-notice/redis/Key.php
+++ app-notice/redis/Key.php
@@ -9,27 +9,28 @@
  */
 abstract class Redis_Key {
 
-	protected static $sPrefix = 'notice:';
-	protected static $aMap = array(
-		'logtable' => 'notice:log:table:$id'
-	);
+    protected static $sPrefix = 'notice:';
+    protected static $aMap = array(
+        'logtable' => 'notice:log:table:{$id}',
+        'mailtable' => 'notice:mail:table:{$id}',
+    );
 
-	public static function __callStatic($name, $args) {
-		$sName = strtolower($name);
-		if (!isset(self::$aMap[$sName])) {
-			return self::autoKey($name);
-		}
-		if (isset($args[0])) {
-			extract($args[0]);
-		}
-		$sKey = self::$aMap[$sName];
-		@eval("\$sKey = \"$sKey\";");
-		return $sKey;
-	}
+    public static function __callStatic($name, $args) {
+        $sName = strtolower($name);
+        if (!isset(self::$aMap[$sName])) {
+            return self::autoKey($name);
+        }
+        if (isset($args[0])) {
+            extract($args[0]);
+        }
+        $sKey = self::$aMap[$sName];
+        @eval("\$sKey = \"$sKey\";");
+        return $sKey;
+    }
 
-	protected static function autoKey($name) {
-		return self::$sPrefix . strtolower(preg_replace('/([a-z])([A-Z])/', '$1:$2', $name));
-	}
+    protected static function autoKey($name) {
+        return self::$sPrefix . strtolower(preg_replace('/([a-z])([A-Z])/', '$1:$2', $name));
+    }
 
 }
 
