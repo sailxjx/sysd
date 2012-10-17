12f8aa9f0dd41bf885c4634fd94203b30d44e210
sailxjx <sailxjx@163.com>
2012-10-17 11:32:32 +0800

    change dir stuctures

--- config/share.php
+++ config/share.php
@@ -2,7 +2,7 @@
 // common configs
 $config['DEBUG'] = 1;
 $config['LOG_FILE'] = APP_PATH . 'var/log/sys.log';
-$config['PID_PATH'] = APP_PATH . 'var/tmp/';
+$config['PID_PATH'] = APP_PATH . 'var/run/';
 $config['MAN_PAGE'] = APP_PATH . 'var/man/man.json';
 $config['HOOK_PATH'] = APP_PATH . 'var/hooks/';
 $config['MAX_DAEMON_NUM'] = 10;
