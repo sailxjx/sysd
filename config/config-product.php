70c9458eefad883415991a27e00df2fe25afdca1
sailxjx <sailxjx@163.com>
2012-11-19 16:48:20 +0800

    edit configs

--- config/config-product.php
+++ config/config-product.php
@@ -7,12 +7,18 @@ $config['REDIS'] = array(
     'port' => 6380
 );
 
-// job lists
-$config['CMD'] = array(
-    'MailServer -d --log-file=' . APP_PATH . 'var/log/mailserver.log',
-    'MailVent -d --log-file=' . APP_PATH . 'var/log/mailvent.log',
-    'MailSink -d --log-file=' . APP_PATH . 'var/log/mailsink.log',
-    'MailRedel -d --log-file=' . APP_PATH . 'var/log/mailredel.log',
+// init job list
+$config['INIT_JOBS'] = array(
+    'Server -d --log-file=' . APP_PATH . 'var/log/server.log'
+);
+
+// sub job lists started by server
+$config['JOBS'] = array(
+    'MailServer -d --log-file=' . APP_PATH . 'var/log/mailserver.log -w',
+    'MailVent -d --log-file=' . APP_PATH . 'var/log/mailvent.log -w',
+    'MailSink -d --log-file=' . APP_PATH . 'var/log/mailsink.log -w',
+    'MailRedel -d --log-file=' . APP_PATH . 'var/log/mailredel.log -w',
+    // 'MailWorker -d --log-file=' . APP_PATH . 'var/log/mailworker.log --daemon-num=3'
 );
 
 // return configs
