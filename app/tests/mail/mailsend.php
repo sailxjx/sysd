9e0f6ce615d58353da235db0a0e7ce11f56f5b25
sailxjx <sailxjx@163.com>
2012-12-17 12:17:45 +0800

    edit mail send test

--- app/tests/mail/mailsend.php
+++ app/tests/mail/mailsend.php
@@ -2,42 +2,24 @@
 function sendMail($aData, $oRedis) {
     $aFields = array(
         'email' => 'email',
-        'title' => 'title',
-        'sender' => 'sender',
-        'receiver' => 'receiver',
-        'mailparams' => 'mailparams',
-        'template' => 'template'
+        'template' => 'template',
+        'servicetype' => 'servicetype',
+        'mailparams' => 'mailparams'
     );
     $aData = array_intersect_key($aData, $aFields);
-    if (count($aData) != count($aFields)) {
-        return false;
-    }
+    print_r($aData);
+    exit;
     return $oRedis->lpush('notice:mail:server', json_encode($aData));
 }
 
 $oRedis = new Redis();
-$oRedis->connect('127.0.0.1', 6379);
+$oRedis->connect('192.168.2.71', 6380);
 $aPost = array(
-    'email' => 'lei.chen@51fanli.com',
-    'sender' => 'bash',
-    'receiver' => 'liushuang',
-    'title' => 'batch test',
-    'username' => '::-)呵呵',
-    'template' => 'test',
-    'age' => 1,
-);
-$aParams = array(
-    'params' => array(
-        'username' => 'hellworld',
-        'pwdurl' => 'http://www.baidu.com',
-        'date' => date('Y-m-d H:i:s')
-    )
+    'email' => 'jingxin.xu@51fanli.com',
+    'template' => 'happybirthday',
+    'servicetype' => 'webpower'
 );
 $aData = array_merge($aPost, array(
     'mailparams' => json_encode($aParams)
 ));
-// $aData['servicetype'] = 'webpower';
-// for ($i = 0;$i < 10;$i++) {
-//    $aData['sender'] = 'bash' . $i;
-    sendMail($aData, $oRedis);
-// }
+sendMail($aData, $oRedis);
