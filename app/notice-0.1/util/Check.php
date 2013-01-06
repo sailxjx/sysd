76a438a1010ac54a7beadf61f28e54489213406c
sailxjx <sailxjx@163.com>
2013-01-06 10:10:15 +0800

    add 7

--- app/notice-0.1/util/Check.php
+++ app/notice-0.1/util/Check.php
@@ -1,6 +1,6 @@
 <?php
 class Util_Check {
     public static function isLegalMobile($sMobile){
-        return preg_match("/^1[3458]\d{9}$/", $sMobile);
+        return preg_match("/^1[34578]\d{9}$/", $sMobile);
     }
 }
